97b2f2ef03f17311a603d4550012f0bbcab7adaf
Adriano Ramos <adriano.ramos3208@gmail.com>
2023-05-24 14:16:50 -0300

    aaaaaaa

--- screens/BatteryInfo.jsx
+++ screens/BatteryInfo.jsx
@@ -54,7 +54,7 @@ export default function BatteryInfo() {
         /> */}
         <View
           style={{
-            paddingHorizontal: 100,
+            paddingHorizontal: 100 ,
           }}
         >
           <ProgressBar

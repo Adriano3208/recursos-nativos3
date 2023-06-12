8839efa6ad043f60a7962f8071194ff990d0457e
Adriano Ramos <adriano.ramos3208@gmail.com>
2023-06-12 16:27:22 -0300

    SOCORRO

--- screens/Notify.jsx
+++ screens/Notify.jsx
@@ -1,9 +1,9 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import * as Notifications from 'expo-notifications';
+import * as Notifications from "expo-notifications";
 import { useState } from "react";
-import * as Device from 'expo-device'
+import * as Device from "expo-device";
 import * as Battery from "expo-battery";
 import { useEffect } from "react";
 
@@ -14,80 +14,145 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function Notify({navigation}) {
+export default function Notify({ navigation }) {
   const [nivelBateria, setNivelBateria] = useState();
   const [nivelCor, setNivelCor] = useState("green");
-
-  const [expoToken, setExpoToken] = useState('');
+  const [ultimaNotif, setUltimaNotif] = useState();
+  const [expoToken, setExpoToken] = useState("");
 
   async function notificarExpo() {
     const token = await Notifications.scheduleNotificationAsync({
       content: {
-        title: 'Minha notificação',
-        subtitle: 'subtitulo',
-        body: 'corpo da notificação',
-
+        title: "Minha notificação",
+        subtitle: "subtitulo",
+        body: "corpo da notificação",
       },
-      trigger: { seconds: 3 }
-    })
+      trigger: { seconds: 3 },
+    });
     setExpoToken(token);
   }
 
   async function notificarBateria() {
-    
     const token = await Notifications.scheduleNotificationAsync({
       content: {
-        title: 'Nivel de Bateria',
-        subtitle: 'subtitulo',
-        body: 'seu nivel de bateria é:' + nivelBateria + '%',
+        title: "Nivel de Bateria",
+        subtitle: "subtitulo",
+        body: "seu nivel de bateria é:" + nivelBateria + "%",
       },
-      trigger: { seconds: 3 }
-    })
+      trigger: { seconds: 3 },
+    });
+
     setExpoToken(token);
   }
 
   async function bateria() {
     const nivel = await Battery.getBatteryLevelAsync();
     setNivelBateria(nivel * 100);
-    
+  }
+
+  const UltimaNotificacao = Notifications.useLastNotificationResponse();
+  async function exibirAlerta() {
+    alert("");
+    console.log(UltimaNotificacao);
+  }
+
+  useEffect(
+    () => {
+      exibirAlerta();
+      bateria();
+    },
+    [UltimaNotificacao],
+    [nivelBateria]
+  );
+
+  const ultimaNotificacao = Notifications.useLastNotificationResponse();
+
+  async function exibirAlerta() {
+    const idToken = ultimaNotificacao.Notifications;
+
+    alert("Atenção! " + idToken);
+
+    console.log(idToken);
   }
 
   useEffect(() => {
-    bateria();
-  }, [nivelBateria]);
+    if (ultimaNotificacao) {
+      setUltimaNotif(ultimaNotificacao.Notifications);
+    }
+
+    exibirAlerta();
+  }, [ultimaNotificacao]);
+
+  async function lerNotificacao() {
+    const ultimaNotificacao =
+      await Notifications.getLastNotificationResponseAsync();
+
+    alert(ultimaNotificacao.Notifications);
+
+    console.log(ultimaNotificacao);
+  }
+
+  async function mudarPagina() {
+    const idToken = ultimaNotif;
+
+    alert("Atenção! " + idToken);
+
+    console.log(idToken);
+
+    if (idToken == expoToken) {
+      navigation.navigate("HomeAula");
+    }
+  }
+
+  useEffect(() => {
+    mudarPagina();
+  }, [ultimaNotificacao]);
 
   return (
     <View style={styles.container}>
       <Header title={"Notificações"} />
       <View>
-        <Text>Expo Token: { expoToken } </Text>
+        <Text>Expo Token: {expoToken} </Text>
         <Button
           title="enviar notificação"
-          onPress={async ()=> {
+          onPress={async () => {
             notificarExpo();
-          }}></Button>
+          }}
+        />
 
-          <Button title="bateria noti"
-          onPress={async ()=> {
+        <Button
+          title="bateria noti"
+          onPress={async () => {
             notificarBateria();
-          }}/>
-          <Button title="seu aparelho"
-            onPress={async ()=> {
+          }}
+        />
+        <Button
+          title="seu aparelho"
+          onPress={async () => {
             const token = await Notifications.scheduleNotificationAsync({
               content: {
-                title: 'Seu Aparelho',
-                subtitle: 'subtitulo',
-                body: 'seu aparelho é:' + Device.modelName,
+                title: "Seu Aparelho",
+                subtitle: "subtitulo",
+                body: "seu aparelho é:" + Device.modelName,
               },
-              trigger: { seconds: 3 }
-            })
+              trigger: { seconds: 3 },
+            });
             setExpoToken(token);
-          }
-        }/>
-          
-          {/* BOTÕES PARA PROXIMA AULA */}
-          <Button title="ler a ultima notificação clicada"/>
-          <Button title="ler a notificação não clicada"/>
+          }}
+        />
+
+<Button
+
+title="Enviar ultima notificação lida"
+
+onPress={async () => lerNotificacao()}/>
+
+<Button
+
+ title="notificar e ir para outra página"
+
+ onPress={async () => mudarPagina()}/>
+
       </View>
       <Footer onPress={() => navigation.back()} />
     </View>

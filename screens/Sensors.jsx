a5e0a3a705ab5e41dd798778e566d35c3ead9a67
Adriano Ramos <adriano.ramos3208@gmail.com>
2023-08-14 14:52:32 -0300

    cucucu

--- screens/Sensors.jsx
+++ screens/Sensors.jsx
@@ -1,14 +1,9 @@
 import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { useState } from "react";
-import { useCallback } from "react";
-import Items from "../components/Items";
-import * as Contacts from "expo-contacts";
-import * as Notifications from "expo-notifications";
-import { useFocusEffect } from "@react-navigation/native";
-import { Accelerometer, Gyroscope, Magnetometer } from "expo-sensors";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Accelerometer, Gyroscope, Magnetometer, Barometer } from "expo-sensors";
+import { ScrollView } from "react-native"; // Import ScrollView
 
 const styles = StyleSheet.create({
   container: {
@@ -17,9 +12,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  titulo: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
 
 export default function Sensors({ navigation }) {
+  const [barometer, setBarometer] = useState(); // Estado para o barômetro
   const [giroscopio, setGiroscopio] = useState();
   const [accelero, setAccelero] = useState(); // Estado para o acelerômetro
   const [magneto, setMagneto] = useState();
@@ -27,11 +27,13 @@ export default function Sensors({ navigation }) {
   const [backgroundColor, setBackgroundColor] = useState("white"); // Estado para a cor de fundo
 
   useEffect(() => {
+    Barometer.addListener(barometerListener); // Corrected import and listener
     Accelerometer.addListener(acceleroListener);
     Gyroscope.addListener(giroscopioListener);
     Magnetometer.addListener(magnetoListener);
 
     return () => {
+      Barometer.removeAllListeners(); // Clean up Barometer listener
       Gyroscope.removeAllListeners();
       Magnetometer.removeAllListeners();
     };
@@ -50,11 +52,11 @@ export default function Sensors({ navigation }) {
 
   const acceleroListener = (data) => {
     setAccelero(data);
-
-  
   };
 
-
+  const barometerListener = (data) => {
+    setBarometer(data);
+  };
 
   const giroscopioListener = (data) => {
     setGiroscopio(data);
@@ -75,29 +77,35 @@ export default function Sensors({ navigation }) {
   };
 
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <Header title={"Sensores"} style={styles.titulo} />
-      <View style={styles.container}>
-        <Text style={styles.titulo}>Giroscopio</Text>
-        <Text style={styles.titulo}>x: {giroscopio?.x}</Text>
-        <Text style={styles.titulo}>y: {giroscopio?.y}</Text>
-        <Text style={styles.titulo}>z: {giroscopio?.z}</Text>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={[styles.container, { backgroundColor }]}>
+        <Header title={"Sensores"} style={styles.titulo} />
+        <View style={styles.container}>
+          <Text style={styles.titulo}>Giroscopio</Text>
+          <Text style={styles.titulo}>x: {giroscopio?.x}</Text>
+          <Text style={styles.titulo}>y: {giroscopio?.y}</Text>
+          <Text style={styles.titulo}>z: {giroscopio?.z}</Text>
 
-        <Text style={styles.titulo}>Magnetometro</Text>
-        <Text style={styles.titulo}>x: {magneto?.x}</Text>
-        <Text style={styles.titulo}>y: {magneto?.y}</Text>
-        <Text style={styles.titulo}>z: {magneto?.z}</Text>
+          <Text style={styles.titulo}>Magnetometro</Text>
+          <Text style={styles.titulo}>x: {magneto?.x}</Text>
+          <Text style={styles.titulo}>y: {magneto?.y}</Text>
+          <Text style={styles.titulo}>z: {magneto?.z}</Text>
 
-        <Text style={styles.titulo}>Accelerometer</Text>
-        <Text style={styles.titulo}>x: {accelero?.x}</Text>
-        <Text style={styles.titulo}>y: {accelero?.y}</Text>
-        <Text style={styles.titulo}>z: {accelero?.z}</Text>
-        
-        
+          <Text style={styles.titulo}>Accelerometer</Text>
+          <Text style={styles.titulo}>x: {accelero?.x}</Text>
+          <Text style={styles.titulo}>y: {accelero?.y}</Text>
+          <Text style={styles.titulo}>z: {accelero?.z}</Text>
 
-        <Text style={styles.titulo}>Orientação: {orientation}</Text>
+          <Text style={styles.titulo}>Barometro</Text>
+          <Text style={styles.titulo}>Pressão: {barometer?.pressure}</Text>
+          <Text style={styles.titulo}>Altitude relativa: {barometer?.relativeAltitude}</Text>
+          <Text style={styles.titulo}>Altitude absoluta: {barometer?.absoluteAltitude}</Text>
+
+          <Text style={styles.titulo}>Orientação: {orientation}</Text>
+        </View>
+        <Footer />
       </View>
-      <Footer />
-    </View>
+    </ScrollView>
   );
 }
+
